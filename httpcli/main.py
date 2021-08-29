7225450d17b727cd99d40717c19afdd9afbac3e1
le_woudar <rollandkev@yahoo.fr>
2021-08-29 10:04:26 +0200

    refactor(main): refactored http command function

--- httpcli/main.py
+++ httpcli/main.py
@@ -1,8 +1,33 @@
+from pathlib import Path
+from typing import Optional, Union
+
 import click
 from click_params import URL
 
 from .configuration import Configuration, Auth
 from .parameters import AUTH_PARAM
+
+
+def set_configuration_options(
+        config: Configuration,
+        proxy: Optional[str] = None,
+        http_version: Optional[str] = None,
+        backend: Optional[str] = None,
+        auth: Optional[Auth] = None,
+        follow_redirects: Optional[bool] = None,
+        verify: Optional[Union[bool, Path]] = True
+) -> None:
+    if http_version is not None:
+        config.version = http_version
+    if backend is not None:
+        config.backend = backend
+    if proxy is not None:
+        config.proxy = proxy
+    if auth is not None:
+        config.auth = auth
+    if follow_redirects is not None:
+        config.follow_redirects = follow_redirects
+    config.verify = verify
 
 
 @click.version_option('0.1.0', message='%(prog)s version %(version)s')
@@ -27,17 +52,7 @@ from .parameters import AUTH_PARAM
 def http(context: click.Context, proxy: str, http_version: str, backend: str, auth: Auth, follow_redirects: bool):
     """HTTP CLI"""
     config = context.ensure_object(Configuration)
-    if http_version is not None:
-        config.version = http_version
-    if backend is not None:
-        config.backend = backend
-    if proxy is not None:
-        config.proxy = proxy
-    if auth is not None:
-        config.auth = auth
-    if follow_redirects is not None:
-        config.follow_redirects = follow_redirects
-    config.verify = False
+    set_configuration_options(config, proxy, http_version, backend, auth, follow_redirects, verify=False)
 
 
 if __name__ == '__main__':
